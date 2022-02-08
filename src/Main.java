211244c7fc3be5828f815d0abb2acba6ba520f68
samah37 <hs_kansab@esi.dz>
2022-02-08 14:22:44 +0100

    Updated: CPU + Memory ops

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,25 +8,21 @@ public class Main {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
         SystemInformation systemInformation = new SystemInformation();
-        systemInformation.StartTime();
-
-
         List<String> list = new ArrayList<String>();
-        for (int i = 0; i <= 500000; i++) {
+        for (int i = 0; i <= 999900; i++) {
             list.add("JimKnopf");
         }
-
-
-
 
         System.out.println("Used memory is megabytes: "
                 + systemInformation.JavaMemoryUsageMB());
        // long stopTime = System.currentTimeMillis();
        // long elapsedTime = stopTime - startTime;
         System.out.println(systemInformation.JavaElapsedTime());
+        System.out.println("test ");
 
-        System.out.println(systemInformation.JavaSystemCpuLoad());
+        System.out.println(systemInformation.JavaProcessCpuTime());
     }}
 
--- src/SystemInformation.java
+++ src/SystemInformation.java
@@ -1,39 +1,54 @@
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 import com.sun.management.OperatingSystemMXBean;
+
+import javax.management.MBeanServerConnection;
+
 public class SystemInformation {
     public long startTime;
     public long endTime;
     private static final long KILOBYTE = 1024L;
-    private static final long MEGABYTE = 1024L * 1024L  ;
-    private static final long GEGABYTE = 1024L * 1024L * 1024L ;
+    private static final long MEGABYTE = 1024L * 1024L;
+    private static final long GEGABYTE = 1024L * 1024L * 1024L;
     public OperatingSystemMXBean operatingSystemMXBean =
             (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    public ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+
+    public SystemInformation() throws IOException {
+        this.startTime = System.currentTimeMillis();
+    }
+
     public static long bytesTokilobytes(long bytes) {
         return bytes / KILOBYTE;
     }
+
     public static long bytesToMegabytes(long bytes) {
         return bytes / MEGABYTE;
     }
+
     public static long bytesToGegabytes(long bytes) {
         return bytes / GEGABYTE;
     }
-   public void StartTime(){
-       this.startTime= System.currentTimeMillis();
-   }
-   public long JavaElapsedTime(){
-       this.endTime = System.currentTimeMillis();
-       return endTime-startTime;
-   }
-   public long JavaMemoryUsageMB(){
-       // Get the Java runtime
-       Runtime runtime = Runtime.getRuntime();
-       // Run the garbage collector
-       runtime.gc();
-       // Calculate the used memory
-       long memory = runtime.totalMemory() - runtime.freeMemory();
-       return bytesToMegabytes(memory);
+
+    public long JavaElapsedTime() {
+        this.endTime = System.currentTimeMillis();
+        return this.endTime - this.startTime;
     }
-    public long JavaMemoryUsageGB(){
+
+    public long JavaMemoryUsageMB() {
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        return bytesToMegabytes(memory);
+
+    }
+
+    public long JavaMemoryUsageGB() {
         // Get the Java runtime
         Runtime runtime = Runtime.getRuntime();
         // Run the garbage collector
@@ -42,18 +57,24 @@ public class SystemInformation {
         long memory = runtime.totalMemory() - runtime.freeMemory();
         return bytesToGegabytes(memory);
     }
-    public long JavaSystemCpuLoad(){
+
+    public long JavaSystemCpuLoad() {
         return (long) this.operatingSystemMXBean.getCpuLoad();
     }
-    public long JavaSystemProcessCpuLoad(){
+
+    public long JavaSystemProcessCpuLoad() {
         return (long) this.operatingSystemMXBean.getProcessCpuLoad();
     }
-    public long JavaProcessCpuTime(){
-        return this.operatingSystemMXBean.getProcessCpuTime();
+
+    public long JavaProcessCpuTime() {
+        long[] allThreadIds = threadMXBean.getAllThreadIds();
+        long nano = 0;
+        for (long id : allThreadIds) {
+            nano += threadMXBean.getThreadCpuTime(id);
+        }
+        return (long) (nano / 1E6);
     }
-
-
-
 
 
 }
+
